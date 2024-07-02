e6db7c72871b114db9fa804a2de57e0efffe3ea4
klh269 <enoch.eko@gmail.com>
2024-07-02 11:52:16 +0100

    Update Vbar_Vobs.py

--- Vbar_Vobs.py
+++ Vbar_Vobs.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
+from tabulate import tabulate
 
 """
 Triggers for different sections of code. Set variable to True for
@@ -13,7 +14,9 @@ finite_diff = True
 spline_fit = True
 plot_splines = True
 plot_d1 = True
+plot_d1_ALL = True
 plot_d2 = True
+plot_d2_ALL = True
 testing = False
 
 file = "C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/SPARC_Lelli2016c.mrt.txt"
@@ -39,17 +42,17 @@ def Vbar(arr):
 
 galaxy_count = len(table["Galaxy"])
 if testing:
-    galaxy_count = 1
+    galaxy_count = 3
 bulged_count = 0
 xbulge_count = 0
 
-fd_corr = {}
-spline_corr = {}
-bulged_corr = {}
-xbulge_corr = {}
-d2_corr = {}
-bulged_corr2 = {}
-xbulge_corr2 = {}
+fd_corr = []
+spline_corr = []
+bulged_corr = []
+xbulge_corr = []
+d2_corr = []
+bulged_corr2 = []
+xbulge_corr2 = []
 
 for i in range(galaxy_count):
     g = table["Galaxy"][i]
@@ -76,7 +79,7 @@ for i in range(galaxy_count):
         dVobs_dr = np.diff(data["Vobs"]) / np.diff(r)
         dVbar_dr = np.diff(Vbar(data)) / np.diff(r)
         corr = np.corrcoef(dVobs_dr, dVbar_dr)[0,1]
-        fd_corr[g] = corr
+        fd_corr.append(corr)
     
     """
     Fit (cubic) splines through data and Vbar (reconstructed).
@@ -92,31 +95,44 @@ for i in range(galaxy_count):
         # First derivative of splines (change 1 to n for nth derivative).
         dVbar = Vbar_fit.derivative(1)
         dVobs = Vobs.derivative(1)
+        dVgas = Vgas.derivative(1)
+        dVdisk = Vdisk.derivative(1)
+        if bulged:
+            dVbul = Vbul.derivative(1)
         
-        # Compute correlation by taking finite differences between nearby points.
+        # Compute correlation of Vbar vs Vobs by taking
+        # finite differences between nearby points.
         rad = np.linspace(0., r[len(r)-1], num=10000)
         dVobs_dr = dVobs(rad)
         dVbar_dr = dVbar(rad)
         corr = np.corrcoef(dVobs_dr, dVbar_dr)[0,1]
-        spline_corr[g] = corr
+        spline_corr.append(corr)
         if bulged:
-            bulged_corr[g] = corr
+            bulged_corr.append(corr)
+            xbulge_corr.append(-np.inf)
         else:
-            xbulge_corr[g] = corr
+            bulged_corr.append(-np.inf)
+            xbulge_corr.append(corr)
         
         # Second derivative of splines.
         d2Vbar = Vbar_fit.derivative(2)
         d2Vobs = Vobs.derivative(2)
+        d2Vgas = Vgas.derivative(2)
+        d2Vdisk = Vdisk.derivative(2)
+        if bulged:
+            d2Vbul = Vbul.derivative(2)
         
         # Compute correlation by taking finite differences between nearby points.
         d2Vobs_dr2 = d2Vobs(rad)
         d2Vbar_dr2 = d2Vbar(rad)
         corr = np.corrcoef(d2Vobs_dr2, d2Vbar_dr2)[0,1]
-        d2_corr[g] = corr
+        d2_corr.append(corr)
         if bulged:
-            bulged_corr2[g] = corr
+            bulged_corr2.append(corr)
+            xbulge_corr2.append(-np.inf)
         else:
-            xbulge_corr2[g] = corr
+            bulged_corr2.append(-np.inf)
+            xbulge_corr2.append(corr)
     
     """
     Plot spline fits for data;
@@ -132,14 +148,14 @@ for i in range(galaxy_count):
         plt.scatter(r, data["Vgas"], color="green", alpha=0.3)
         plt.scatter(r, data["Vdisk"]*np.sqrt(pdisk), color="blue", alpha=0.3)
         
-        plt.plot(r, Vobs(r), color='k', label="Vobs")
-        plt.plot(r, Vbar_fit(r), color='red', label="Vbar")
-        plt.plot(r, Vgas(r), color="green", label="Vgas")
-        plt.plot(r, Vdisk(r)*np.sqrt(pdisk), color="blue", label="Vdisk")
+        plt.plot(rad, Vobs(rad), color='k', label="Vobs")
+        plt.plot(rad, Vbar_fit(rad), color='red', label="Vbar")
+        plt.plot(rad, Vgas(rad), color="green", label="Vgas")
+        plt.plot(rad, Vdisk(rad)*np.sqrt(pdisk), color="blue", label="Vdisk")
         
         if bulged:
             plt.scatter(r, data["Vbul"]*np.sqrt(pbul), color="darkorange", alpha=0.3)
-            plt.plot(r, Vbul(r)*np.sqrt(pbul), color="darkorange", label="Vbul")
+            plt.plot(rad, Vbul(rad)*np.sqrt(pbul), color="darkorange", label="Vbul")
         
         plt.legend(bbox_to_anchor=(1,1), loc="upper left")
         plt.savefig("C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/d0/"+g+".png", dpi=300, bbox_inches="tight")
@@ -147,7 +163,7 @@ for i in range(galaxy_count):
         plt.close()
     
     """
-    Plot first derivatives of the splines;
+    Plot first derivatives of the splines for Vobs and Vbar;
     Trigger by setting plot_d1 = True in top of code.
     """
     if plot_d1:
@@ -158,13 +174,13 @@ for i in range(galaxy_count):
         ax1.set_xlabel("Radius (kpc)")
         ax1.set_ylabel("Velocities (km/s)", color=color)
     
-        ln1 = ax1.plot(r, dVbar(r), color=color, label="Baryonic curve - Vbar")
+        ln1 = ax1.plot(rad, dVbar(rad), color=color, label="Baryonic curve - Vbar")
         ax1.tick_params(axis='y', labelcolor=color)
         
         ax2 = ax1.twinx()
         color = "black"
         ax2.set_ylabel("Velocities (km/s)", color=color)
-        ln2 = ax2.plot(r, dVobs(r), color=color, label="Total curve - Vobs")
+        ln2 = ax2.plot(rad, dVobs(rad), color=color, label="Total curve - Vobs")
         ax2.tick_params(axis='y', labelcolor=color)
         
         lns = ln1 + ln2
@@ -175,6 +191,28 @@ for i in range(galaxy_count):
         plt.show()
         
         fig1.savefig("C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/d1/"+g+".png", dpi=300, bbox_inches="tight")
+        plt.close()
+    
+    """
+    Plot first derivatives of ALL the splines;
+    Trigger by setting plot_d1_ALL = True in top of code.
+    """
+    if plot_d1_ALL:
+        plt.title("First derivative: "+g)
+        plt.xlabel('Radius (kpc)')
+        plt.ylabel('Velocities (km/s)')
+        
+        plt.plot(rad, dVobs(rad), color='k', label="Vobs")
+        plt.plot(rad, dVbar(rad), color='red', label="Vbar")
+        plt.plot(rad, dVgas(rad), color="green", label="Vgas")
+        plt.plot(rad, dVdisk(rad)*np.sqrt(pdisk), color="blue", label="Vdisk")
+        
+        if bulged:
+            plt.plot(rad, dVbul(rad)*np.sqrt(pbul), color="darkorange", label="Vbul")
+        
+        plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+        plt.savefig("C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/d1_ALL/"+g+".png", dpi=300, bbox_inches="tight")
+        plt.show()
         plt.close()
         
     """
@@ -189,13 +227,13 @@ for i in range(galaxy_count):
         ax1.set_xlabel("Radius (kpc)")
         ax1.set_ylabel("Velocities (km/s)", color=color)
     
-        ln1 = ax1.plot(r, d2Vbar(r), color=color, label="Baryonic curve - Vbar")
+        ln1 = ax1.plot(rad, d2Vbar(rad), color=color, label="Baryonic curve - Vbar")
         ax1.tick_params(axis='y', labelcolor=color)
         
         ax2 = ax1.twinx()
         color = "black"
         ax2.set_ylabel("Velocities (km/s)", color=color)
-        ln2 = ax2.plot(r, d2Vobs(r), color=color, label="Total curve - Vobs")
+        ln2 = ax2.plot(rad, d2Vobs(rad), color=color, label="Total curve - Vobs")
         ax2.tick_params(axis='y', labelcolor=color)
         
         lns = ln1 + ln2
@@ -207,27 +245,62 @@ for i in range(galaxy_count):
         
         fig2.savefig("C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/d2/"+g+".png", dpi=300, bbox_inches="tight")
         plt.close()
+        
+        """
+        Plot first derivatives of ALL the splines;
+        Trigger by setting plot_d1_ALL = True in top of code.
+        """
+        if plot_d2_ALL:
+            plt.title("Second derivative: "+g)
+            plt.xlabel('Radius (kpc)')
+            plt.ylabel('Velocities (km/s)')
+            
+            plt.plot(rad, d2Vobs(rad), color='k', label="Vobs")
+            plt.plot(rad, d2Vbar(rad), color='red', label="Vbar")
+            plt.plot(rad, d2Vgas(rad), color="green", label="Vgas")
+            plt.plot(rad, d2Vdisk(rad)*np.sqrt(pdisk), color="blue", label="Vdisk")
+            
+            if bulged:
+                plt.plot(rad, d2Vbul(rad)*np.sqrt(pbul), color="darkorange", label="Vbul")
+            
+            plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+            plt.savefig("C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/d2_ALL/"+g+".png", dpi=300, bbox_inches="tight")
+            plt.show()
+            plt.close()
 
+# Write all correlations into a table and save to txt file (correlations.txt, same directory as the plots)
+corr_arrays = np.array([table["Galaxy"], fd_corr, spline_corr, bulged_corr, xbulge_corr, d2_corr, bulged_corr2, xbulge_corr2])
+corr_arrays = np.transpose(corr_arrays)
+corr_arrays = corr_arrays.reshape(galaxy_count,8)
+header = [ "Galaxy", "Finite difference", "Spline: 1st derivative",
+            "w/ bulge","w/o bulge", "Spline: 2nd derivative", "w/ bulge", "w/o bulge" ]
+corr_table = pd.DataFrame(corr_arrays, columns=header)
+
+corr_table.to_csv('C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/correlations.txt', sep='\t', index=False)
+with open('C:/Users/admin/OneDrive/Desktop/Other/Oxford UROP 2024/plots/Vbar_Vobs/corr_table.txt', 'w') as f:
+    f.write(tabulate(corr_table, headers=header))
+
+# Some useful info regarding percentages of positive correlations.
 print("Using finite difference:")
-corr_pos = [c for c in fd_corr.values() if c > 0]
+corr_pos = [c for c in fd_corr if c > 0]
 pos_rate = len(corr_pos) / galaxy_count
 print("Total number of galaxies =", galaxy_count)
 print("Number of positive correlations =", len(corr_pos), ", i.e.", round(pos_rate*100, 1), "% of all galaxies.")
 
 print()
 print("Using (cubic) spline fits:")
-corr_pos = [c for c in spline_corr.values() if c > 0]
+corr_pos = [c for c in spline_corr if c > 0]
 pos_rate = len(corr_pos) / galaxy_count
 print("No. of positive 1st derivative correlations =", len(corr_pos), ", i.e.", round(pos_rate*100, 1), "% of all galaxies.")
-bulged_pos = [c for c in bulged_corr.values() if c > 0]
-xbulge_pos = [c for c in xbulge_corr.values() if c > 0]
+bulged_pos = [c for c in bulged_corr if c > 0]
+xbulge_pos = [c for c in xbulge_corr if c > 0]
 print("Out of these, there are {} galaxies with bulge ({}%) and {} without ({}%)."
       .format(len(bulged_pos), round(len(bulged_pos)/bulged_count*100, 1), len(xbulge_pos), round(len(xbulge_pos)/xbulge_count*100, 1)))
 
-corr2_pos = [c for c in d2_corr.values() if c > 0]
+corr2_pos = [c for c in d2_corr if c > 0]
 pos_rate = len(corr2_pos) / galaxy_count
 print("No. of positive 2nd derivative correlations =", len(corr2_pos), ", i.e.", round(pos_rate*100, 1), "% of all galaxies.")
-bulged_pos = [c for c in bulged_corr2.values() if c > 0]
-xbulge_pos = [c for c in xbulge_corr2.values() if c > 0]
+bulged_pos = [c for c in bulged_corr2 if c > 0]
+xbulge_pos = [c for c in xbulge_corr2 if c > 0]
 print("Out of these, there are {} galaxies with bulge ({}%) and {} without ({}%)."
       .format(len(bulged_pos), round(len(bulged_pos)/bulged_count*100, 1), len(xbulge_pos), round(len(xbulge_pos)/xbulge_count*100, 1)))
