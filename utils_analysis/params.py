1206a959057e9d3fc3a3f4d4ee3cf21b5de34c62
klh269 <enoch.eko@gmail.com>
2024-08-28 21:58:16 +0800

    Update params.py

--- utils_analysis/params.py
+++ utils_analysis/params.py
@@ -9,3 +9,8 @@ G = 4.300e-6    # Gravitational constant G (kpc (km/s)^2 solarM^(-1))
 pdisk, pbul = 0.5, 0.7  # Mean mass-to-light ratios for disk and bulge.
 a0 = 1.2e-10 / 3.24e-14     # Scale acceleration for MOND [pc/yr^2].
 num_samples = 100   # Number of Vbar (hence mock Vobs) samples to generate.
+
+# Parameters from Richard's code:
+GNEWTON = 4.30091727e-06    # km^2 kpc Msun^(-1) s^-2
+RHO200C = 200 * 277.54      # Msun / kpc^3  (h = 1)
+LITTLE_H = 0.7
