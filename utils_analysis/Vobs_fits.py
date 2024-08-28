68ec318eec2fd28908fa9b357ea9cc4eb8dc80d0
klh269 <enoch.eko@gmail.com>
2024-08-28 21:58:11 +0800

    Update Vobs_fits.py

--- utils_analysis/Vobs_fits.py
+++ utils_analysis/Vobs_fits.py
@@ -4,12 +4,14 @@ Dark matter halo fits using different models in LCDM.
 """
 import numpy as np
 from scipy.integrate import quad
+from quadax import cumulative_trapezoid, fixed_quadgk
 
 from jax import numpy as jnp
 from numpyro import distributions as dist
 from numpyro import deterministic, sample, factor
+from numpyro.distributions.transforms import AffineTransform
 
-from .params import G, pdisk, pbul, a0
+from .params import G, pdisk, pbul, a0, RHO200C, LITTLE_H
 
 
 # Calculate baryonic matter from data of individual galaxies.
@@ -39,27 +41,85 @@ def gNFWhalo_vsq(r, rho0, rc, alpha):
     v_sq = G * Mr / r
     return v_sq
 
-def MOND_Vobs(r, Vbar2, a0=a0):
+def MOND_vsq(r, Vbar2, a0=a0):
     # Quadratic solution from MOND simple interpolating function.
     acc = Vbar2 / r
-    y = acc / a0
-    nu = 1 + np.sqrt((1 + 4/y))
+    y = jnp.array(acc / a0)
+    nu = 1 + jnp.sqrt((1 + 4/y))
     nu /= 2
-    return np.sqrt(acc * nu * r)
+    return acc * nu * r
 
 
-# Main code for sampling Vbar uncertainties and fit to Vobs.
-def Vobs_fit(table, i_table, data, bulged, profile="NFW"):
+######################################################################################
+# CODE FROM RICHARD: https://github.com/Richard-Sti/RCfit/blob/master/rcfit/model.py #
+######################################################################################
+
+def M200c2R200c(M200c):
+    """
+    Convert M200c [Msun] to R200c [kpc] using the definition of the critical
+    density `RHO200C` (which defines the units of `M200c` and `R200c`).
+    """
+    return (3 * M200c / (4 * jnp.pi * RHO200C * LITTLE_H**2))**(1./3)
+
+
+def NFW_velocity_squared(r, M200c, c):
+    """
+    Squared circular velocity of an NFW profile at radius `r` for a halo with
+    mass `M200c` and concentration `c`.
+    """
+    R200c = M200c2R200c(M200c)
+    Rs = R200c / c
+    return G * M200c / r * (jnp.log(1 + r / Rs) - r / (r + Rs)) / (jnp.log(1 + c) - c / (1 + c))
+
+
+def gNFW_velocity_squared(r, M200c, c, alpha, beta, gamma):
+    """
+    Generalized NFW rotation velocity squared, calculated by numerically
+    integrating the density profile.
+    """
+    R200c = M200c2R200c(M200c)
+    Rs = R200c / c
+
+    def integrand(x):
+        return x**2 / (x**alpha * (1 + x**beta)**gamma)
+
+    xmin, xmax = r[0] / Rs, r[-1] / Rs
+    x = jnp.logspace(jnp.log10(xmin), jnp.log10(xmax), 2048)
+
+    # Integrand from 0 to minial range.
+    ymin = fixed_quadgk(integrand, 0., xmin)[0]
+    # Normalizattoin constant
+    norm = fixed_quadgk(integrand, 0., c)[0]
+
+    # Integrate from xmin to xmax.
+    ycum = cumulative_trapezoid(integrand(x), x) + ymin
+    ycum = jnp.concatenate([jnp.array([ymin]), ycum])
+
+    # Calculate the mass enclosed within r.
+    Mr = M200c * jnp.interp(r / Rs, x, ycum) / norm
+    return G * Mr / r
+
+
+def transformed_normal(loc, scale):
+    return dist.TransformedDistribution(
+        dist.Normal(0, 1), AffineTransform(loc=loc, scale=scale))
+
+
+##############################################################
+# Main code for sampling Vbar uncertainties and fit to Vobs. #
+##############################################################
+
+def Vobs_fit(table, i_table, data, bulged, profile):
     """
     Code for fitting halo profile to observed Vobs + Vbar.
     A major part of this references model.py from Richard Stiskalek:
     https://github.com/Richard-Sti/RCfit/blob/master/rcfit/model.py
     """
     # Sample halo parameters.
-    if profile in ["Iso", "NFW", "gNFW"]:
-        Rmax = max(data["Rad"])
-        rho0 = sample("rho0", dist.Uniform(0., 1.0e5))
-        rc = sample("Rc", dist.Uniform(0., 100*Rmax))
+    # if profile in ["Iso", "NFW", "gNFW"]:
+        # Rmax = max(data["Rad"])
+        # rho0 = sample("rho0", dist.Uniform(0., 1.0e5))
+        # rc = sample("Rc", dist.Uniform(0., 100*Rmax))
     
     # Sample mass-to-light ratios.
     smp_pdisk = sample("Disk M/L", dist.Normal(pdisk, 0.125))
@@ -70,7 +130,7 @@ def Vobs_fit(table, i_table, data, bulged, profile="NFW"):
 
     # # Sample inclination and scale Vobs
     # inc_min, inc_max = 15 * jnp.pi / 180, 150 * jnp.pi / 180
-    # inc = sample("inc",dist.TruncatedNormal(table["Inc"][i_table], table["e_Inc"][i_table], low=inc_min, high=inc_max))  # noqa
+    # inc = sample("inc",dist.TruncatedNormal(table["Inc"][i_table], table["e_Inc"][i_table], low=inc_min, high=inc_max))
     # inc_scaling = jnp.sin(table["Inc"][i_table]) / jnp.sin(inc)
     # Vobs = deterministic("Vobs", jnp.array(data["Vobs"]) * inc_scaling)
     # e_Vobs = deterministic("e_Vobs", jnp.array(data["errV"]) * inc_scaling)
@@ -93,18 +153,22 @@ def Vobs_fit(table, i_table, data, bulged, profile="NFW"):
     # Calculate the predicted velocity.
     r = deterministic("r", jnp.array(data["Rad"]) * dist_scaling)
     
-    if profile == "Iso":
-        Viso_squared = ISOhalo_vsq(r, rho0, rc)
-        Vpred = deterministic("Vpred", jnp.sqrt(Viso_squared + Vbar_squared))
-    elif profile == "NFW":
-        Vnfw_squared = NFWhalo_vsq(r, rho0, rc)
+    # if profile == "Iso":
+    #     Viso_squared = ISOhalo_vsq(r, rho0, rc)
+    #     Vpred = deterministic("Vpred", jnp.sqrt(Viso_squared + Vbar_squared))
+    if profile == "NFW":
+        logM200c = sample("logM200c", transformed_normal(12.0, 2.0))
+        logc_mean = 0.905 - 0.101 * (logM200c - 12. + jnp.log10(LITTLE_H))
+        logc = sample("logc", transformed_normal(logc_mean, 0.11))
+        # Vnfw_squared = NFWhalo_vsq(r, rho0, rc)
+        Vnfw_squared = NFW_velocity_squared(r, 10**logM200c, 10**logc)
         Vpred = deterministic("Vpred", jnp.sqrt(jnp.array(Vnfw_squared + Vbar_squared)))
-    elif profile == "gNFW":
-        alpha = sample("alpha", dist.Uniform(0., 10.))
-        Vgnfw_squared = gNFWhalo_vsq(r, rho0, rc, alpha)
-        Vpred = deterministic("Vpred", jnp.sqrt(Vgnfw_squared + Vbar_squared))
+    # elif profile == "gNFW":
+    #     alpha = sample("alpha", dist.Uniform(0., 10.))
+    #     Vgnfw_squared = gNFWhalo_vsq(r, rho0, rc, alpha)
+    #     Vpred = deterministic("Vpred", jnp.sqrt(Vgnfw_squared + Vbar_squared))
     elif profile == "MOND":
-        Vpred = deterministic("Vpred", MOND_Vobs(r, Vbar_squared))
+        Vpred = deterministic("Vpred", jnp.sqrt(MOND_vsq(r, Vbar_squared)))
     else:
         raise ValueError(f"Unknown profile: '{profile}'.")
     
@@ -115,7 +179,7 @@ def Vobs_fit(table, i_table, data, bulged, profile="NFW"):
 
 
 def BIC_from_samples(samples, log_likelihood,
-                     skip_keys=["Vobs", "Vpred", "e_Vobs", "log_likelihood", "r"]):  # noqa
+                     skip_keys=["Vobs", "Vpred", "e_Vobs", "log_likelihood", "r"]):
     """
     Get the BIC from HMC samples of a Numpyro model.
 
@@ -146,7 +210,7 @@ def BIC_from_samples(samples, log_likelihood,
         elif val.ndim == 2:
             nparam += val.shape[-1]
         else:
-            raise ValueError("Invalid dimensionality of samples to count the number of parameters.")  # noqa
+            raise ValueError("Invalid dimensionality of samples to count the number of parameters.")
 
     BIC = nparam * np.log(ndata) - 2 * log_likelihood[kmax]
 
