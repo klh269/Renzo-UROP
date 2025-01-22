474f68d6b475d058fec25604a883e84dfe15b210
klh269 <enoch.eko@gmail.com>
2025-01-22 16:03:49 +0000

    Update extract_ft.py

--- utils_analysis/extract_ft.py
+++ utils_analysis/extract_ft.py
@@ -12,56 +12,90 @@ def find_consecutive(arr, stepsize=1):
     return np.split(arr, np.where(np.diff(arr) != stepsize)[0]+1)
 
 def ft_check(arr, errV, min_height:float=2.0):
-    idx_pos = np.where( arr > 0.0 )[0]
-    idx_peaks = find_consecutive( idx_pos )
-    idx_neg = np.where( arr < 0.0 )[0]
-    idx_troughs = find_consecutive( idx_neg )
+    mask = np.abs(arr) >= errV * min_height
+    if np.count_nonzero(mask) == 0:
+        return [], [], []
+    
+    arr_normalized = np.zeros_like(arr)
+    arr_normalized[mask] = arr[mask] / errV[mask]
+    
+    significant_indices = np.where( np.abs(arr_normalized) > min_height )[0]
+    idx_ft = find_consecutive( significant_indices )
+    arr_ft = np.where( np.abs(arr_normalized) > min_height )[0]
+    idx_ft = find_consecutive( arr_ft )
 
-    lb_peaks, rb_peaks = [], []
-    for segment in idx_peaks:
-        if np.count_nonzero( np.array(arr[segment]) > min_height * np.array(errV[segment]) ) >= 1 and len(segment) >= 3:
-            lb_peaks.append( segment[0] )
-            rb_peaks.append( segment[-1] )
-
-    lb_troughs, rb_troughs = [], []
-    for segment in idx_troughs:
-        if np.count_nonzero( np.array(arr[segment]) < - min_height * np.array(errV[segment]) ) >= 1 and len(segment) >= 3:
-            lb_troughs.append( segment[0] )
-            rb_troughs.append( segment[-1] )
-            
     lb_ft, rb_ft = [], []
-    if len(lb_troughs) >= 1 and len(lb_peaks) >= 1:
-        # Check if peaks and troughs can be joined together to form a larger feature (wiggle).
-        bases = list(itertools.chain(lb_peaks, rb_peaks, lb_troughs, rb_troughs))
-        bases.sort()
-        ft_bases = []
-        i = 0
-        while i < len(bases)-1:
-            if bases[i+1] - bases[i] != 1:
-                ft_bases.append(bases[i])
-                i += 1
-            else:
+    for segment in idx_ft:
+        condition = np.abs(np.array(arr_normalized[segment])) > min_height
+        if np.count_nonzero(condition) >= 3:
+            lb_ft.append(segment[0])
+            rb_ft.append(segment[-1])
+
+    if len(lb_ft) == 0:
+        return [], [], []
+
+    lb_features, rb_features = [], []
+    i = 0
+    while i < len(lb_ft):
+        lb_features.append(lb_ft[i])
+        if i + 1 < len(lb_ft):
+            if lb_ft[i+1] - rb_ft[i] <= 1:
+                rb_features.append(idx_ft[i+1][-1])
                 i += 2
-        ft_bases.append(bases[-1])
+            else:
+                rb_features.append(rb_ft[i])
+                i += 1
+        else:
+            rb_features.append(idx_ft[i][-1])
+            i += 1
 
-        for ftb in ft_bases:
-            lb_ft.append(ftb) if ftb in lb_peaks or ftb in lb_troughs else rb_ft.append(ftb)
+    return lb_features, rb_features, [rb_features[i] - lb_features[i] for i in range(len(lb_features))]
 
-    elif len(lb_troughs) >= 1:
-        lb_ft = lb_troughs
-        rb_ft = rb_troughs
-    elif len(lb_peaks) >= 1:
-        lb_ft = lb_peaks
-        rb_ft = rb_peaks
+    # lb_peaks, rb_peaks = [], []
+    # for segment in idx_peaks:
+    #     if np.count_nonzero( np.array(arr_normalized[segment]) > min_height ) >= 3: # and len(segment) >= 3:
+    #         lb_peaks.append( segment[0] )
+    #         rb_peaks.append( segment[-1] )
 
-    lb_features, rb_features, widths = [], [], []
-    for ib in range(len(lb_ft)):
-        if rb_ft[ib] - lb_ft[ib] >= 4:
-            lb_features.append(lb_ft[ib])
-            rb_features.append(rb_ft[ib])
-            widths.append(rb_ft[ib] - lb_ft[ib])
+    # lb_troughs, rb_troughs = [], []
+    # for segment in idx_troughs:
+    #     if np.count_nonzero( np.array(arr_normalized[segment]) < - min_height ) >= 3: # and len(segment) >= 3:
+    #         lb_troughs.append( segment[0] )
+    #         rb_troughs.append( segment[-1] )
+            
+    # lb_ft, rb_ft = [], []
+    # if len(lb_ft) >= 1:
+    #     # Check if peaks and troughs can be joined together to form a larger feature (wiggle).
+    #     bases = list(itertools.chain(lb_ft, rb_ft))
+    #     bases.sort()
+    #     ft_bases = []
+    #     i = 0
+    #     while i < len(bases)-1:
+    #         if bases[i+1] - bases[i] != 1:
+    #             ft_bases.append(bases[i])
+    #             i += 1
+    #         else:
+    #             i += 2
+    #     ft_bases.append(bases[-1])
 
-    return lb_features, rb_features, widths
+    #     for ftb in ft_bases:
+    #         lb_ft.append(ftb) if ftb in lb_peaks or ftb in lb_troughs else rb_ft.append(ftb)
+
+    # elif len(lb_troughs) >= 1:
+    #     lb_ft = lb_troughs
+    #     rb_ft = rb_troughs
+    # elif len(lb_peaks) >= 1:
+    #     lb_ft = lb_peaks
+    #     rb_ft = rb_peaks
+
+    # lb_features, rb_features, widths = [], [], []
+    # for ib in range(len(lb_ft)):
+    #     if rb_ft[ib] - lb_ft[ib] >= 4:
+    #         lb_features.append(lb_ft[ib])
+    #         rb_features.append(rb_ft[ib])
+    #         widths.append(rb_ft[ib] - lb_ft[ib])
+
+    # return lb_features, rb_features, widths
 
 
 # def ft_check(arr, errV):
